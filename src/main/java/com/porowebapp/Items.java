32dff2280a1b4a8d2d586acf53e0da48a4a1ba99
8Cookie9 <Cookie3141592653589>
2017-03-24 19:29:03 +0200

    fixes

--- src/main/java/com/porowebapp/Items.java
+++ src/main/java/com/porowebapp/Items.java
@@ -3,78 +3,78 @@ package com.porowebapp;
 import java.util.Random;
 
 public class Items {
+
     private String miscCom;
     private String miscTxt;
     private String headCom;
     private String headTxt;
-    
-    public Items(){
+
+    public Items() {
     }
-    
-    public void getMiscC(String m, int randseed){
-        String[] miscellaneous=m.split("-");
-        String result="misc "+miscellaneous[1];
-        if(Integer.parseInt(miscellaneous[0])==1){
-            result+=" "+1;
-        }else if(Integer.parseInt(miscellaneous[0])==2){
-            result+=" "+2;
-     
-       }if(Integer.parseInt(miscellaneous[0])==3){
-            result+=" "+(new Random(randseed).nextInt(2)+3);
-        }else{
-            result+=" "+(new Random(randseed).nextInt(2)+5);
+
+    public void getMiscC(String m, int randseed) {
+        String[] miscellaneous = m.split("-");
+        String result = "misc " + miscellaneous[1];
+        if (Integer.parseInt(miscellaneous[0]) == 1) {
+            result += " " + 1;
+        } else if (Integer.parseInt(miscellaneous[0]) == 2) {
+            result += " " + 2;
+        } else if (Integer.parseInt(miscellaneous[0]) == 3) {
+            result += " " + (new Random(randseed).nextInt(2) + 3);
+        } else {
+            result += " " + (new Random(randseed).nextInt(2) + 5);
         }
-        this.miscCom=result;
+        this.miscCom = result;
     }
-    
-    public void getMiscT(String m, int randseed){
-        String[] miscellaneous=m.split("-");
-        String result=" received "+miscellaneous[1];
-        if(Integer.parseInt(miscellaneous[0])==1){
-            result+=" (+"+1+" hp).";
-        }else if(Integer.parseInt(miscellaneous[0])==2){
-            result+=" (+"+2+" hp).";
-     
-       }if(Integer.parseInt(miscellaneous[0])==3){
-            result+=" (+"+(new Random(randseed).nextInt(2)+3)+" hp).";
-        }else{
-            result+=" (+"+(new Random(randseed).nextInt(2)+5)+" hp).";
+
+    public void getMiscT(String m, int randseed) {
+        String[] miscellaneous = m.split("-");
+        String result = " received " + miscellaneous[1];
+        if (Integer.parseInt(miscellaneous[0]) == 1) {
+            result += " (+" + 1 + " hp).";
+        } else if (Integer.parseInt(miscellaneous[0]) == 2) {
+            result += " (+" + 2 + " hp).";
+
+        } else if (Integer.parseInt(miscellaneous[0]) == 3) {
+            result += " (+" + (new Random(randseed).nextInt(2) + 3) + " hp).";
+        } else {
+            result += " (+" + (new Random(randseed).nextInt(2) + 5) + " hp).";
         }
-        this.miscTxt=result;
+        this.miscTxt = result;
     }
-    
-    public void getHeadgearC(String h, int randseed){
-        String[] miscellaneous=h.split("-");
-        for(int i=0;i<miscellaneous.length;i++){
+
+    public void getHeadgearC(String h, int randseed) {
+        String[] miscellaneous = h.split("-");
+        for (int i = 0; i < miscellaneous.length; i++) {
             System.out.println(miscellaneous[i]);
         }
-        String result="headgear "+miscellaneous[1];
-        if(Integer.parseInt(miscellaneous[0])==1){
-            result+=" "+1;
-        }else if(Integer.parseInt(miscellaneous[0])==2){
-            result+=" "+2;
-        }if(Integer.parseInt(miscellaneous[0])==3){
-            result+=" "+(new Random(randseed).nextInt(2)+3);
-        }else{
-            result+=" "+(new Random(randseed).nextInt(2)+5);
+        String result = "headgear " + miscellaneous[1];
+        if (Integer.parseInt(miscellaneous[0]) == 1) {
+            result += " " + 1;
+        } else if (Integer.parseInt(miscellaneous[0]) == 2) {
+            result += " " + 2;
+        } else if (Integer.parseInt(miscellaneous[0]) == 3) {
+            result += " " + (new Random(randseed).nextInt(2) + 3);
+        } else {
+            result += " " + (new Random(randseed).nextInt(2) + 5);
         }
-        this.headCom=result;
+        this.headCom = result;
     }
-    
-    public void getHeadgearT(String h, int randseed){
-        String[] miscellaneous=h.split("-");
-        String result=" received "+miscellaneous[1];
-        if(Integer.parseInt(miscellaneous[0])==1){
-            result+=" (+"+1+" defense).";
-        }else if(Integer.parseInt(miscellaneous[0])==2){
-            result+=" (+"+2+" defense).";
-     
-       }if(Integer.parseInt(miscellaneous[0])==3){
-            result+=" (+"+(new Random(randseed).nextInt(2)+3)+" defense).";
-        }else{
-            result+=" (+"+(new Random(randseed).nextInt(2)+5)+" defense).";
+
+    public void getHeadgearT(String h, int randseed) {
+        String[] miscellaneous = h.split("-");
+        String result = " received " + miscellaneous[1];
+        if (Integer.parseInt(miscellaneous[0]) == 1) {
+            result += " (+" + 1 + " defense).";
+        } else if (Integer.parseInt(miscellaneous[0]) == 2) {
+            result += " (+" + 2 + " defense).";
+
+        } else if (Integer.parseInt(miscellaneous[0]) == 3) {
+            result += " (+" + (new Random(randseed).nextInt(2) + 3) + " defense).";
+        } else {
+            result += " (+" + (new Random(randseed).nextInt(2) + 5) + " defense).";
         }
-        this.headTxt=result;
+        this.headTxt = result;
     }
 
     public String getMiscCom() {
@@ -92,6 +92,5 @@ public class Items {
     public String getHeadTxt() {
         return headTxt;
     }
-    
-    
+
 }
